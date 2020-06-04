f41eddb97181b975ec02f341a6f48e989167ead0
kaybrian <kayonbrian@gmail.com>
2020-06-04 12:43:52 +0300

    blog model is done

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.CharField(max_length=250)
+    description = models.CharField(max_length=340)
+    date_Created = models.DateField(auto_now_add=True)
+    date_Updated = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title

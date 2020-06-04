f101a175c05646ac7edd1d9756e0210211adb489
kaybrian <kayonbrian@gmail.com>
2020-06-04 12:59:04 +0300

    view blog part is done

--- blog/serializer.py
+++ blog/serializer.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Blog
 
 
-class BlogSerializer(serializers.ModelSerializers):
+class BlogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Blog
         fields = "__all__"
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework.response import Response 
 from rest_framework.decorators import api_view
-
+from .serializer import BlogSerializer
+from .models import Blog
 # Create your views here.
 @api_view(['GET'])
 def home(request):
@@ -13,3 +14,9 @@ def home(request):
         'View-blog Details':'/view-blog-detail/<str:pk>/',
     }
     return Response(api_urls)
+
+@api_view(['GET'])
+def ViewBlogs(request):
+    blogs = Blog.objects.all()
+    serialiser = BlogSerializer(blogs, many=True)
+    return Response(serialiser.data)

4b1273e8205b202e13bd6e0f56bc46cfa5504ed7
kaybrian <kayonbrian@gmail.com>
2020-06-04 12:52:09 +0300

    worked on the models serializer

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=150)
     author = models.CharField(max_length=250)
-    description = models.CharField(max_length=340)
+    description = models.TextField(max_length=340)
     date_Created = models.DateField(auto_now_add=True)
     date_Updated = models.DateField(auto_now=True)
 

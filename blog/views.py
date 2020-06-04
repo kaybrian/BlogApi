174aebf9d424cdd4a9203c9029edece6951c1f43
kaybrian <kayonbrian@gmail.com>
2020-06-04 12:33:39 +0300

    the home page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.response import Response 
+from rest_framework.decorators import api_view
 
 # Create your views here.
+def home(request):
+    api_urls = {
+        'Create':'/create-blog/',
+        'Update':'/update-blog/<str:pk>/',
+        'Create':'/create-blog/<str:pk>/',
+        'View-blogs':'/view-blog-list/',
+    }
+    return Response('This is a blog api response',safe=False)
